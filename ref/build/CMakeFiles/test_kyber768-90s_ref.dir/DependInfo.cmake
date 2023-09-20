6b20d45f80aec4ce9a4fde7c239d1918622d3d77
aditd <aditdaone@gmail.com>
2023-09-20 18:13:07 +0530

    deleted irrelevant stuff

---
+++ ref/build/CMakeFiles/test_kyber768-90s_ref.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/rdx/kyber/ref/randombytes.c" "CMakeFiles/test_kyber768-90s_ref.dir/randombytes.o" "gcc" "CMakeFiles/test_kyber768-90s_ref.dir/randombytes.o.d"
+  "/Users/rdx/kyber/ref/test_kyber.c" "CMakeFiles/test_kyber768-90s_ref.dir/test_kyber.o" "gcc" "CMakeFiles/test_kyber768-90s_ref.dir/test_kyber.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
